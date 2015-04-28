9a98e637da98c3303a3618b40d6d0437b8e4c070
Nik Yusof <nikyusof@bu.edu>
2015-04-27 22:18:55 -0400

    Modified the Best Fit
    - Got the Best Fit algorithm working.

--- src/caseTwo.java
+++ src/caseTwo.java
@@ -4,35 +4,36 @@
  *
  */
 public class caseTwo extends cases {
-	
+
 	/***Constructor***/
 	public caseTwo(Queue processes, MainMemory mainMemory) {
 		this.processes = processes;
 		this.mainMemory = mainMemory;
 	}
 
-	/***Run the OS on Case Two***/
+	/***Execute the OS on Case Two***/
 	@Override
 	public void run() {
 		while (!caseTerminated) {
 			if (verbose) { System.out.println("I'm running!"); }
-			// Assign Jobs to memory
+			
+			//Assign Jobs to memory
 			Boolean assigned = false;
 			int count = 0;
 			
 			while (!assigned) {
-				// If there are no available positions in the memory left
+				//If there are no available positions in the memory left
 				if (!mainMemory.memoryAvailable()) {
 					assigned = true;
 				}
 
-				// If there are no more Jobs that have not been completed or currently assigned
+				//If there are no more Jobs that have not been completed or currently assigned
 				if (!processes.getUnassignedProcess()) {
 					assigned = true;
 				}
 
-				// If every job has been checked, but for whatever reason (i.e. none of the jobs
-				// can fit in the currently available memory) a Job cannot be assigned.
+				//If every job has been checked, but for whatever reason (i.e. none of the jobs
+				//can fit in the currently available memory) a Job cannot be assigned.
 				if (count >= processes.getLength()) {
 					assigned = true;
 				}
@@ -45,12 +46,14 @@ public class caseTwo extends cases {
 						if (processes.getStatus(count) == "Waiting") {
 							testDone = true;
 						}
-					} else {
+					}
+
+					else {
 						assigned = true;
 						testDone = true;
 					}
 
-					if (testDone == false) {
+					if(testDone == false) {
 						count++;
 					}
 				}
@@ -58,39 +61,36 @@ public class caseTwo extends cases {
 
 				// Begin assigning jobs until the above conditions are no longer true.
 				if (!assigned){
-					// This section should be skipped if assigned is true.
+					// This section should be skipepd if assigned is true.
 					if (count < processes.getLength()) {
-						// During this unit, there are still Jobs that haven't been checked that may need to be assigned.
+						// During this tick, there are still Jobs that haven't been checked that may need to be assigned.
 						Boolean success = false;
+						
+						int minimum = Integer.MAX_VALUE;
+						int index = -1;
 						
 						for (int i = mainMemory.firstAvailableMemorySlot(); i < mainMemory.size && success == false; i++) {
 							if (!mainMemory.getInUse(i)) {
 								// This memory module is not in use.
 								if (processes.getMemoryRequest(count) < mainMemory.getSize(i)) {
 									if (verbose) { System.out.println("A"); }
-									int[] temp = new int[mainMemory.size];
 									
-									for (int j = 0; j < mainMemory.size; j++) {
-										temp[j] = mainMemory.sizeArray[j] - processes.getMemoryRequest(j);
+									if (mainMemory.sizeArray[i] - processes.getMemoryRequest(count) < minimum && mainMemory.sizeArray[i] - processes.getMemoryRequest(count) >= 0) {
+										minimum = mainMemory.sizeArray[i] - processes.getMemoryRequest(count);
+										index = i;
 									}
 
 									if (verbose) { System.out.println("B"); }
-									int currentSmallest = 0;
-									
-									for (int j = 0; currentSmallest + j < temp.length; j++) {
-										if (temp[currentSmallest + j] < temp[currentSmallest] && temp[currentSmallest + j] >= 0) {
-											currentSmallest = currentSmallest + j;
-											j = 0;
-										}
-									}
 
-									if (verbose) { System.out.println("C"); }
-									// This job can fit in the memory block
-									mainMemory.assignMemory(i, processes.getProcess(count));
-									success = true;
+									if (verbose) { System.out.println("C"); }					
 								}
 							}
-						}	    
+						}
+						
+						if (index != -1) {
+							mainMemory.assignMemory(index, processes.getProcess(count));
+							success = true;
+						}
 					}
 				}
 
@@ -103,23 +103,23 @@ public class caseTwo extends cases {
 				System.out.println(mainMemory.toString());
 			}
 
-			// Increase the tick by 1.
+			//Increase the tick by 1.
 			timeUnit();
 
-			// Break the while loop if all jobs are done.
-			if (!processes.getUnfinishedProcess()) {
+			//Break the while loop if all jobs are done.
+			if(!processes.getUnfinishedProcess()) {
 				caseTerminated = true;
 			}
 
-			// Break the while loop if one more tick will be the 30th (max) tick, per the instructions.
-			// Comment this out if you wish for the program to execute until ALL jobs reach a "Finished" state.
+			//Break the while loop if one more tick will be the 30th (max) tick, per the instructions.
+			//Comment this out if you wish for the program to execute until ALL jobs reach a "Finished" state.
 			if (countoftimeSliceFull >= TIME_LIMIT) {
 				caseTerminated = true;
 			}
 		}
 
-		// Now that this case has executed, output the total number of jobs completed.
+		//Now that this case has executed, output the total number of jobs completed.
 		numberOfFinishedProcess = processes.numberOfFinishedProcess();
-		System.out.println("Total number of finished processes: " + numberOfFinishedProcess);
+		System.out.println("Total number of finished jobs: " + numberOfFinishedProcess);
 	}
 }
